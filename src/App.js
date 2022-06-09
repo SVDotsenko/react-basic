ccd2ba28aa30ed54637c15e0d351e7198ef3f834
Serhii Dotsenko <serdotsenko@gmail.com>
2022-06-09 08:55:07 +0200

    разбитие на компоненты: функциональные и классовые

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import {useState} from "react";
+import ClassCounter from "./components/ClassCounter";
 
 export default function App() {
-    const [likes, setLikes] = useState(5);
-    const [value, setValue] = useState('Текс в инпуте');
-
-    function increment() {
-        setLikes(likes + 1);
-    }
-
-    function decrement() {
-        setLikes(likes - 1);
-    }
-
     return (
         <div className="App">
-            <h1>{likes}</h1>
-            <h1>{value}</h1>
-            <input
-                type="text"
-                value={value}
-                onChange={event => setValue(event.target.value)}
-            />
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
+            <ClassCounter/>
         </div>
     );
 }
