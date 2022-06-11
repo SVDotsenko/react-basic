f6e5296677f59fb709293bd4cd5945b383646dcb
Serhii Dotsenko <serdotsenko@gmail.com>
2022-06-11 11:58:29 +0200

    реализован механизм удаления постов

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {useState} from "react";
 import PostList from "./components/PostList";
-import './styles/App.css'
 import PostForm from "./components/PostForm";
+import './styles/App.css'
 
 export default function App() {
     const [posts, setPosts] = useState([
@@ -14,10 +14,17 @@ export default function App() {
         setPosts([...posts, newPost]);
     }
 
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id));
+    }
+
     return (
         <div className="App">
             <PostForm create={createPost}/>
-            <PostList posts={posts} title="список постов про JS"/>
+            <PostList remove={removePost}
+                      posts={posts}
+                      title="Посты про JS"
+            />
         </div>
     );
 }
