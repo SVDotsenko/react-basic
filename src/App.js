a0515bd2b07d1a0f1ddebae2d8e49b52ce60717a
Serhii Dotsenko <serdotsenko@gmail.com>
2022-06-11 10:15:20 +0200

    передача параметров от ребёнка к родителю

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import {useState} from "react";
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
 import './styles/App.css'
+import PostForm from "./components/PostForm";
 
 export default function App() {
     const [posts, setPosts] = useState([
@@ -11,31 +10,13 @@ export default function App() {
         {id: 3, title: 'JavaScript 3', body: 'Description'}
     ]);
 
-    const [post, setPost] = useState({title: '', body: ''});
-
-    const addNewPost = (e) => {
-        e.preventDefault();
-        setPosts([...posts, {...post, id: Date.now()}]);
-        setPost({title: '', body: ''});
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost]);
     }
 
     return (
         <div className="App">
-            <form>
-                <MyInput
-                    value={post.title}
-                    onChange={e => setPost({...post, title: e.target.value})}
-                    type="text"
-                    placeholder="название поста"
-                />
-                <MyInput
-                    value={post.body}
-                    onChange={e => setPost({...post, body: e.target.value})}
-                    type="text"
-                    placeholder="описание поста"
-                />
-                <MyButton onClick={addNewPost}>создать пост</MyButton>
-            </form>
+            <PostForm create={createPost}/>
             <PostList posts={posts} title="список постов про JS"/>
         </div>
     );
